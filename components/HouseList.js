649f24470f3b039d20957f2e95580131cefeec6f
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-10-14 09:05:52 +0200

    Fetching houses from json api.

--- components/HouseList.js
+++ components/HouseList.js
@@ -1,21 +1,19 @@
 import HouseRow from './HouseRow';
-
-const houses = [
-  {
-    id: 1,
-    address: "12 Valley of kings, Geneva",
-    country: "Switzerland",
-    price: 900000
-  },
-  {
-    id: 2,
-    address: "89 Road of forks, Bern",
-    country: "Switzerland",
-    price: 500000
-  },
-]
+import {useEffect, useState} from 'react';
 
 const HouseList = () => {
+  const [houses, setHouses] = useState([]);
+
+  const addHouse = (house) => {
+    setHouses(prev => [...prev, house]);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/houses")
+      .then(res => res.json())
+      .then(data => setHouses(data));
+  }, [])
+
   return <>
     <div className="row mb-2">
       <h5 className="theme-font-color text-center">Houses Currently on the market</h5>
@@ -32,6 +30,7 @@ const HouseList = () => {
         {houses.map(house => <HouseRow key={house.id} {...house} />)}
       </tbody>
     </table>
+    <button onClick={() => addHouse({id:3, address: "Slagstavägen 2", country: "Sweden", price: 10000})} className="btn btn-primary">Add house</button>
   </>
 }
 
